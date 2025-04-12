758857b2d62ca57a483ad7d0a21030c956e9118a
Justin Chen <justin7.chen@gmail.com>
2025-04-12 11:56:16 -0700

    Added test code for gyro

---
+++ device_script/lsm6dsox_test.ino
@@ -0,0 +1,230 @@
+// Basic demo for accelerometer & gyro readings from Adafruit
+// LSM6DSOX sensor
+
+#include <Adafruit_LSM6DSOX.h>
+
+// For SPI mode, we need a CS pin
+#define LSM_CS 10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+Adafruit_LSM6DSOX sox;
+
+// global var...
+float yawAngle = 0;
+
+// Get angle offset values
+float *xDegOffset = NULL;
+float *yDegOffset = NULL;
+float *zDegOffset = NULL;
+
+// Setting up gyro sensor config
+sensors_event_t accel;
+sensors_event_t gyro;
+sensors_event_t temp;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit LSM6DSOX test!");
+
+  if (!sox.begin_I2C()) {
+    // if (!sox.begin_SPI(LSM_CS)) {
+    // if (!sox.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+    // Serial.println("Failed to find LSM6DSOX chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DSOX Found!");
+
+  // sox.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (sox.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+  // sox.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
+  Serial.print("Gyro range set to: ");
+  switch (sox.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    break; // unsupported range for the DSOX
+  }
+
+  // sox.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (sox.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  // sox.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Gyro data rate set to: ");
+  switch (sox.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  // Something from the Adafruit's tutorial???
+  sox.getEvent(&accel, &gyro, &temp);
+
+  // Calibrating gyro
+  calibrate(gyro, xDegOffset, yDegOffset, zDegOffset);
+}
+
+void loop() {
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  // Serial.print("\t\tAccel X: ");
+  // Serial.print(accel.acceleration.x);
+  // Serial.print(" \tY: ");
+  // Serial.print(accel.acceleration.y);
+  // Serial.print(" \tZ: ");
+  // Serial.print(accel.acceleration.z);
+  // Serial.println(" m/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  yawAngle += rawYawAngle(gyro) - *zDegOffset;
+  Serial.print(rawYawAngle(gyro));
+}
+
+void calibrate(sensors_event_t gyroSensor, float* degX, float* degY, float* degZ)
+{
+  Serial.print("currently calibrating...");
+  // Basically getting the average values, after timePasses seconds of reading
+
+  unsigned long timePassed = 10 * 1000; // calibration time
+  unsigned long timeStart = millis();
+
+  // Acumulators for the average readings
+  float xTotal = 0;
+  float yTotal = 0;
+  float zTotal = 0;
+
+  // Counter for how many readings occured
+  int counter = 0;
+
+  while (timeStart - millis() < timePassed)
+  {
+    xTotal += gyroSensor.gyro.x;
+    yTotal += gyroSensor.gyro.y;
+    zTotal += gyroSensor.gyro.z;
+
+    counter++;
+  }
+
+  // Comput averages, store to pointers
+  *degX = xTotal / counter;
+  *degY = yTotal / counter;
+  *degZ = zTotal / counter;
+
+  Serial.print("finish calibrating...");
+}
+
+float rawYawAngle(sensors_event_t gyroSensor)
+{
+  // For yaw angle, only focus on z-axis.
+
+  // Get timeStart to later get change of time
+  unsigned long timeStart = millis();
+
+  // Read the z value
+  float angleRate = gyroSensor.gyro.z;
+
+
+  // (d_theta / dt) * (dt) ~= theta
+  return angleRate * (millis() - timeStart);
+}
